9205c6bf6a9ce34095e746f665c0bfd6f7884ea9
BALU2000-creator <20pa1a0504@vishnu.edu.in>
2025-10-16 15:50:52 +0530

    Visitors 51

--- src/app/main/irains-dashboard/dashboard-maincontainer/dashboard-maincontainer.component.ts
+++ src/app/main/irains-dashboard/dashboard-maincontainer/dashboard-maincontainer.component.ts
@@ -12,7 +12,7 @@ export class DashboardMaincontainerComponent implements OnInit {
   @ViewChild('mapNavBar') mapNavBarComponent!: MapNavBarComponent;
 
   selectedLayer = 'subdivision';
-  showComparison = false;
+  showComparison = true;
   lastActiveLayer = 'subdivision';
   startDate = '';
   endDate = '';
